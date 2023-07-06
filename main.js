8f3a498738c805f2d8958ec9f3f292010a3ac48f
erane <eranyo@mta.ac.il>
2023-07-06 13:44:27 +0300

    server check

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ var cors = require('cors')
 
 const app = express();
 
+const port = 8000
+
 app.use(cors())
 
 app.use(express.json());
 
 app.use('/api/vehicles', userRouter);
 
-app.listen(8000);
+app.listen(port, () =>{
+    console.log(`Started on port ${port}`)
+});
